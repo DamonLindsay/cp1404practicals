47c49900d036f987a6bb64b98e933f80cc5a2eaf
DamonLindsay <damonides@gmail.com>
2023-09-22 10:31:03 +1000

    Update print_stars function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -7,21 +7,20 @@ MINIMUM_PASSWORD_LENGTH = 5
 def main():
     """This program will get a password from the user, and then print a number of *'s equal to the
     password's length."""
-    password = get_password()
+    password = get_password(MINIMUM_PASSWORD_LENGTH)
 
     print_stars(password)
 
 
 def print_stars(password):
-    """Print a number of *'s matching the length of the given argument."""
-    for character in password:
-        print("*", end="")
+    """Print a number of *'s matching the length of the given password."""
+    print("*" * len(password))
 
 
-def get_password():
+def get_password(minimum_length):
     """Get a valid password from the user."""
     password = input("Enter a password: ")
-    while len(password) < MINIMUM_PASSWORD_LENGTH:
+    while len(password) < minimum_length:
         print("Invalid password.")
         password = input("Enter a password: ")
     return password
