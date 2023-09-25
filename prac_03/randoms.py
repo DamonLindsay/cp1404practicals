e28a91eb11358275c7d6f85ca0a897fc7e87607e
DamonLindsay <damonides@gmail.com>
2023-09-26 09:36:18 +1000

    Update comments, add final coding

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+"""
+Randoms - Prac 03
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+# Shortest number is 5, longest is 20.
+
+print(random.randrange(3, 10, 2))  # line 2
+# Shortest number is 3, largest is 9.
+
+print(random.uniform(2.5, 5.5))  # line 3
+# Shortest number is 2.5, largest is 5.4999999999
+
+print(random.randint(1, 100))
