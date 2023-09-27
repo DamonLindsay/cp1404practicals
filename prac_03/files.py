69bf91bdc4a5b61643047fafba80c1f5c6f9c1f4
DamonLindsay <damonides@gmail.com>
2023-09-27 11:22:11 +1000

    Update constant names

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,27 +1,28 @@
 """
 Files - Prac 03
 """
-FILE = "name.txt"
+NAME_FILE = "name.txt"
+NUMBERS_FILE = "numbers.txt"
 
 
 def question_01():
     """Answer for question 1 for files.py."""
     name = input("Name: ")
 
-    with open(FILE, "w") as out_file:
+    with open(NAME_FILE, "w") as out_file:
         print(f"{name}", file=out_file)
 
 
 def question_02():
     """Answer for question 2 for files.py."""
-    with open(FILE, "r") as in_file:
+    with open(NAME_FILE, "r") as in_file:
         name = in_file.readline().strip()
         print(f"Your name is {name}.")
 
 
 def question_03():
     """Answer for question 3 for files.py"""
-    with open("numbers.txt", "r") as in_file:
+    with open(NUMBERS_FILE, "r") as in_file:
         first_number = int(in_file.readline().strip())
         second_number = int(in_file.readline().strip())
         total = first_number + second_number
