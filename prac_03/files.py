f4ee6a606d38b4c4086f650b9c982f1ffdbb90f2
DamonLindsay <damonides@gmail.com>
2023-09-27 11:37:18 +1000

    Rename functions into imperative mood, correct docstrings and add main

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,41 +5,52 @@ NAME_FILE = "name.txt"
 NUMBERS_FILE = "numbers.txt"
 
 
-def question_01():
-    """Answer for question 1 for files.py."""
+def main():
+    """This program contains questions 1-4."""
+    # Question 1
+    write_name_to_file(NAME_FILE)
+
+    # Question 2
+    print_from_file(NAME_FILE)
+
+    # Question 3
+    total = add_first_two_numbers_from_file(NUMBERS_FILE)
+    print(total)
+
+    # Question 4
+    total = add_all_numbers_from_file(NUMBERS_FILE)
+    print(total)
+
+
+def write_name_to_file(filename):  # Question 1
+    """Write the users name to the specified file."""
     name = input("Name: ")
 
-    with open(NAME_FILE, "w") as out_file:
+    with open(filename, "w") as out_file:
         print(f"{name}", file=out_file)
 
 
-def question_02():
-    """Answer for question 2 for files.py."""
-    with open(NAME_FILE, "r") as in_file:
+def print_from_file(filename):  # Question 2
+    """Print the name from the specified file."""
+    with open(filename, "r") as in_file:
         name = in_file.read().strip()
         print(f"Your name is {name}.")
 
 
-def question_03():
-    """Answer for question 3 for files.py"""
-    with open(NUMBERS_FILE, "r") as in_file:
+def add_first_two_numbers_from_file(filename):  # Question 3
+    """Take the first two numbers from a specified file, add them and then return the total."""
+    with open(filename, "r") as in_file:
         first_number = int(in_file.readline().strip())
         second_number = int(in_file.readline().strip())
         total = first_number + second_number
-        print(total)
+        return total
 
 
-def question_04():
-    """Answer for question 4 in files.py."""
+def add_all_numbers_from_file(filename):  # Question 4
+    """Add all numbers from a specified file, then return the combined total."""
     total = 0
-    with open(NUMBERS_FILE, "r") as in_file:
+    with open(filename, "r") as in_file:
         for line in in_file:
             number = int(line.strip())
             total += number
-        print(total)
-
-
-question_01()
-question_02()
-question_03()
-question_04()
+        return total
