66f26491e7f7e506d3009830bdf9243d205da503
DamonLindsay <damonides@gmail.com>
2023-09-27 10:42:46 +1000

    Add answers to the questions asked in the docstring

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,14 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+1a. The ValueError will occur when a non-integer is entered into the numerator or denominator inputs.
+
 2. When will a ZeroDivisionError occur?
+2a. This error will occur when the fraction calculation occurs, if the denominator = 0.
+
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+3a. Yes, you can add a condition that checks if the denominator = 0 before the calculation is attempted.
+If the denominator = 0, display an error message and then ask the user to enter a valid number.
 """
 
 try:
