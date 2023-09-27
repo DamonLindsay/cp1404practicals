23aa4246c1ff9353f3d85394ad7fbe02405f9c50
DamonLindsay <damonides@gmail.com>
2023-09-27 12:09:20 +1000

    Change line that number_of_days variable is stored

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -20,10 +20,11 @@ out_file = open(OUTPUT_FILE, "w")
 
 price = INITIAL_PRICE
 print(f"Starting price: ${price:,.2f}", file=out_file)
-
 number_of_days = 0
+
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    number_of_days += 1
 
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
@@ -36,7 +37,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
 
-    number_of_days += 1
     price *= (1 + price_change)
     print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
 
