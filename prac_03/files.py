dd9406098ecd7d3cc8cf8490f2d8272cc5e3e389
DamonLindsay <damonides@gmail.com>
2023-09-27 11:11:41 +1000

    Add question 2 answer

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,6 +3,7 @@ Files - Prac 03
 """
 FILE = "name.txt"
 
+
 def question_01():
     """Answer for question 1 for files.py."""
     name = input("Name: ")
@@ -11,8 +12,12 @@ def question_01():
         print(f"{name}", file=out_file)
 
 
-# def question_02():
-#     """Answer for question 2 for files.py."""
-#     with open("")
+def question_02():
+    """Answer for question 2 for files.py."""
+    with open(FILE, "r") as in_file:
+        name = in_file.readline().strip()
+        print(f"Your name is {name}.")
 
-question_01()
+
+# question_01()
+question_02()
