edf6d45ac40c38ea013127989ee68484ed032690
DamonLindsay <damon.lindsay@my.jcu.edu.au>
2023-09-29 10:21:31 +1000

    Add random_thing.py file

---
+++ prac_03/random_thing.py
@@ -0,0 +1,18 @@
+"""
+Random Things - Extension
+"""
+
+import random
+
+
+def main():
+    """This program will generate a random Boolean in 3 different ways."""
+    random_number = random.randint(1, 2)
+    print(random_number == 1)
+
+    print(random.choice([True, False]))
+
+    print(bool(random.getrandbits(1)))
+
+
+main()
