16a61efd2db9a233b1594e15c73a06eb6eb6b009
DamonLindsay <damon.lindsay@my.jcu.edu.au>
2023-09-29 10:50:04 +1000

    Update constants to random variables

--- prac_03/more_random_conrad.py
+++ prac_03/more_random_conrad.py
@@ -9,20 +9,20 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.175  # 17.5%
-MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 1.00
-MAX_PRICE = 100.00
-INITIAL_PRICE = 10.0
+random_maximum_increase = random.uniform(0.075, 0.175)  # Random float between 7.5% and 17.5%
+random_maximum_decrease = random.uniform(0.01, 0.05)  # Random float between 1% and 5%
+random_minimum_price = random.uniform(0.01, 1.00)
+random_maximum_price = random.uniform(50.00, 100.00)
+random_initial_price = random.uniform(10.00, 50.00)
 OUTPUT_FILE = "capitalist_conrad_output_file.txt"
 
 out_file = open(OUTPUT_FILE, "w")
 
-price = INITIAL_PRICE
+price = random_initial_price
 print(f"Starting price: ${price:,.2f}", file=out_file)
 number_of_days = 0
 
-while MIN_PRICE <= price <= MAX_PRICE:
+while random_minimum_price <= price <= random_maximum_price:
     price_change = 0
     number_of_days += 1
 
@@ -31,11 +31,11 @@ while MIN_PRICE <= price <= MAX_PRICE:
     if random.randint(1, 2) == 1:
         # generate a random floating-point number
         # between 0 and MAX_INCREASE
-        price_change = random.uniform(0, MAX_INCREASE)
+        price_change = random.uniform(0, random_maximum_increase)
     else:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
+        price_change = random.uniform(-random_maximum_decrease, 0)
 
     price *= (1 + price_change)
     print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
