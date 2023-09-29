df184ca952441244510f14f2e50c8063a4502166
DamonLindsay <damon.lindsay@my.jcu.edu.au>
2023-09-29 11:52:43 +1000

    Add function for getting a valid character

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -8,11 +8,25 @@ MAXIMUM_NUMBER_VALUE = 127
 
 def main():
     """This program will take a character input and see the corresponding ASCII code, and vice versa."""
-    character = input("Enter a character: ")
-    print(f"The ASCII code for {character} is {ord(character)}.")
+    character = get_valid_character()
+    print(f"The ASCII code for '{character}' is '{ord(character)}'.")
 
     user_number = get_valid_number(MINIMUM_NUMBER_VALUE, MAXIMUM_NUMBER_VALUE)
-    print(f"The character for {user_number} is {chr(user_number)}.")
+    print(f"The character for '{user_number}' is '{chr(user_number)}'.")
+
+
+def get_valid_character():
+    """Get a valid character as an input."""
+    is_valid_character = False
+    while not is_valid_character:
+        character = input("Enter a character: ")
+        if len(character) < 1 or len(character) > 1:
+            print("Invalid input.  Only a single character must be entered.")
+        elif character.isalpha():
+            is_valid_character = True
+        else:
+            is_valid_character = True
+    return character
 
 
 def get_valid_number(minimum, maximum):
