8643cc8aeb228016f220eb4521db33876a4cacec
DamonLindsay <damonides@gmail.com>
2023-10-04 09:11:13 +1000

    Add questions 1-4

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -30,3 +30,19 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 
 # numbers + [6, 5, 3]
 # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+print(numbers)
+
+# Question 1
+numbers[0] = "ten"
+print(numbers)
+
+# Question 2
+numbers[-1] = 1
+print(numbers)
+
+# Question 3
+print(numbers[2:])
+
+# Question 4
+print(9 in numbers)
