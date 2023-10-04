20d43ea6638d147e1c029428e9e1669c6d001ed1
DamonLindsay <damonides@gmail.com>
2023-10-04 10:33:00 +1000

    Add list comprehension for extracting big numbers (>9)

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -36,8 +36,8 @@ almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 numbers =[int(number) for number in almost_numbers]
 print(numbers)
 
-# TODO: list comprehension to create a list of only the numbers that are
-# greater than 9 from the numbers (not strings) you just created
+big_numbers = [number for number in numbers if number > 9]
+print(big_numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
