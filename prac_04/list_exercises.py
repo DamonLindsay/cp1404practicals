0491ceb12c5a8925f8a8d0223453368f8c38303b
DamonLindsay <damonides@gmail.com>
2023-10-06 13:12:32 +1000

    Change extension task into comments, leaving prac_04 basic list operations original question intact

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,6 +6,7 @@ NUMBER_OF_NUMBERS = 5
 
 def main():
     """This program prompts the use for numbers, then outputs information about these numbers."""
+    # Question 1
     numbers = []
     get_list_of_numbers(numbers)
     print(f"The first number is {numbers[0]}")
@@ -14,6 +15,7 @@ def main():
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
+    # Question 2
     usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
                  'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
                  'bob']
@@ -21,15 +23,20 @@ def main():
     validate_username(username, usernames)
 
 
-def get_list_of_numbers(numbers):
-    """Get a list of numbers."""
-    number = int(input(f"Number {len(numbers) + 1}: "))
-    while number != 0:
+def get_list_of_numbers(numbers):  # Question 1
+    """Get a list of numbers from the user."""
+    for i in range(5):
+        number = int(input("Number: "))
         numbers.append(number)
-        number = int(input(f"Number {len(numbers) + 1}: "))
+
+    # Extension Exercise (Get numbers until 0 is entered)
+    # number = int(input(f"Number {len(numbers) + 1}: "))
+    # while number != 0:
+    #     numbers.append(number)
+    #     number = int(input(f"Number {len(numbers) + 1}: "))
 
 
-def validate_username(username, usernames):
+def validate_username(username, usernames):  # Question 2
     """Check if the username is in the list of usernames."""
     if username in usernames:
         print("Access granted.")
