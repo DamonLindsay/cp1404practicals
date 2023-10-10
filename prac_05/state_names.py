b8adb12e0776b99179d0857d61f0ccbdeb2ce621
DamonLindsay <damonides@gmail.com>
2023-10-11 09:07:01 +1000

    Add loop that prints all states and names with string formatting

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,10 +4,13 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
+
+max_code_character_length = (max(len(code) for code in CODE_TO_NAME))
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<{max_code_character_length}} is {name}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
