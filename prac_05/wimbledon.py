782a387e438f1c074e123eff0a707726cc41889c
DamonLindsay <damonides@gmail.com>
2023-10-11 11:04:32 +1000

    Add read_wimbledon_data function to wimbledon.py

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,6 +11,8 @@ def main():
     wimbledon_champion_to_win_count = {}
     countries_that_won_wimbledon = set()
 
+    wimbledon_data = read_wimbledon_data(FILENAME)
+
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         next(in_file)
         for row in in_file:
@@ -29,6 +31,18 @@ def main():
     print()
     print(f"These {len(countries_that_won_wimbledon)} countries have won Wimbledon: ")
     print(", ".join(list(sorted(countries_that_won_wimbledon))))
+
+
+def read_wimbledon_data(filename):
+    """Read the Wimbledon data from a file, skipping the first line and returning relevant data."""
+    wimbledon_data = []
+
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        next(in_file)
+        for row in in_file:
+            row = row.strip().split(",")
+            wimbledon_data.append(row)
+    return wimbledon_data
 
 
 main()
