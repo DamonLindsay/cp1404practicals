d2c8a93e657dfb7045942af03b4389e3c07d24e0
DamonLindsay <damonides@gmail.com>
2023-10-11 11:30:24 +1000

    Update state_names.py with functions

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,19 +3,35 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
-
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-max_code_character_length = (max(len(code) for code in CODE_TO_NAME))
-for code, name in CODE_TO_NAME.items():
-    print(f"{code:<{max_code_character_length}} is {name}")
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    try:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    except KeyError:
-        print("Invalid short state")
+def main():
+    """ """
+    print(CODE_TO_NAME)
+
+    max_code_character_length = calculate_max_code_character_length(CODE_TO_NAME)
+    display_states(CODE_TO_NAME, max_code_character_length)
+
     state_code = input("Enter short state: ").upper()
+    while state_code != "":
+        try:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+        except KeyError:
+            print("Invalid short state")
+        state_code = input("Enter short state: ").upper()
+
+
+def calculate_max_code_character_length(code_to_name):
+    """Calculate the maximum character length of state codes in the dictionary."""
+    return max(len(code) for code in code_to_name)
+
+
+def display_states(code_to_name, max_code_character_length):
+    """Display the state codes and names neatly formatted."""
+    for code, name in code_to_name.items():
+        print(f"{code:<{max_code_character_length}} is {name}")
+
+
+main()
