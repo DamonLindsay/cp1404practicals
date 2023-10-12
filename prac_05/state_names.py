d14c9c04ecda36ab1072eeccf30cd525a4097d4a
DamonLindsay <damonides@gmail.com>
2023-10-12 13:04:31 +1000

    Update state_names.py with reviewers code recommendations

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,11 +8,12 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 
 
 def main():
-    """ """
+    """This program will display a dictionary of state codes and their corresponding state names, then prompts the
+     to enter a state code, and displays the corresponding state name."""
     print(CODE_TO_NAME)
 
-    max_code_character_length = calculate_max_code_character_length(CODE_TO_NAME)
-    display_states(CODE_TO_NAME, max_code_character_length)
+    max_char_length = calculate_max_char_length(CODE_TO_NAME)
+    display_states(CODE_TO_NAME, max_char_length)
 
     state_code = input("Enter short state: ").upper()
     while state_code != "":
@@ -23,9 +24,10 @@ def main():
         state_code = input("Enter short state: ").upper()
 
 
-def calculate_max_code_character_length(code_to_name):
+def calculate_max_char_length(code_to_name):
     """Calculate the maximum character length of state codes in the dictionary."""
-    return max(len(code) for code in code_to_name)
+    max_code_character_length = max(len(code) for code in code_to_name)
+    return max_code_character_length
 
 
 def display_states(code_to_name, max_code_character_length):
