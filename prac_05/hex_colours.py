ec369f82d5f81f6bc96fee42ab7fd07b5ec44a06
jc299447 <damon.lindsay@my.jcu.edu.au>
2023-10-13 10:26:16 +1000

    Update dictionary keys to be correctly formatted

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,7 +2,7 @@
 Hex Colours - Prac 05
 
 Based on the state name example program above, create a program that allows you to look up hexadecimal colour codes
-like those at http://www.color-hex.com/color-names.html
+like those at https://www.color-hex.com/color-names.html
 
 Use a constant dictionary of about 10 colour names and write a program that allows a user to enter a name and get
 the code, e.g., entering AliceBlue (or aliceblue - make it case-independent) should show #f0f8ff.
@@ -11,16 +11,16 @@ Entering an invalid colour name should not crash the program.
 Allow the user to enter names until they enter a blank one to stop the loop.
 """
 
-COLOUR_NAME_TO_COLOUR_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff",
+COLOUR_NAME_TO_COLOUR_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
                               "Alizarin Crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00",
-                              "Amethyst": "#9966cc", "Antique White": "#faebd7", "Apricot": "#fbceb1",
+                              "Amethyst": "#9966cc", "AntiqueWhite": "#faebd7", "Apricot": "#fbceb1",
                               "Aqua": "#00ffff"}
 
-
-colour_name = input("Enter a colour name: ").title()
+colour_name = input("Enter a colour name: ")
 while colour_name != "":
     try:
         print(f"{colour_name} is {COLOUR_NAME_TO_COLOUR_CODE[colour_name]}.")
     except KeyError:
         print("Invalid colour name.")
-    colour_name = input("Enter a colour name: ").title()
+    colour_name = input("Enter a colour name: ")
+
