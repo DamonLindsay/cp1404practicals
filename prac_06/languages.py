4d4236c696216adb679266bb550ea8d3a2109797
DamonLindsay <damonides@gmail.com>
2023-10-28 16:43:40 +1000

    Add list of programming languages and dynamic typing loop

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -9,3 +9,13 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+print()  # Separate initial part of program from 2nd half.
+
+programming_languages = [ProgrammingLanguage("Python", "Dynamic", True, 1991),
+                         ProgrammingLanguage("Ruby", "Dynamic", True, 1995),
+                         ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
+
+print("The dynamically typed languages are: ")
+for programming_language in programming_languages:
+    if programming_language.is_dynamic():
+        print(programming_language.name)
