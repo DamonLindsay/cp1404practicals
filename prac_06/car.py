0e59e1a3f083a454eac88d93cbb632fd912db2bf
DamonLindsay <damonides@gmail.com>
2023-10-28 16:24:03 +1000

    Update car.py and used_cars.py with modifications from practical_06 modifications exercise

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,24 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    # Mod 6: Now add a name field to the Car class (in car.py), and adjust the __init__ and __str__ methods to set and
+    # display this respectively.
+    def __init__(self, name="", fuel=0, ):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
+
+    # Mod 5: Now add the __str__ method to the Car class in car.py.
+    # Using f-string formatting. Make it return a string in the following format:
+    # Car, fuel=42, odometer=277
+    # Mod 6: Make the str method return the car's name instead of just "Car".
+    def __str__(self):
+        """Return the string version of the Car object."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,22 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    # Mod 1: Create a new Car object called "limo" that is initialised with 100 units of fuel.
+    limo = Car("Limo", 100)
+    # Mod 2: Add 20 more units of fuel to this new car object using the add method.
+    limo.add_fuel(20)
+    # Mod 3: Print the amount of fuel in the car.
+    print(f"Limo has fuel: {limo.fuel}")
+    # Mod 4: Attempt to drive the car 115 km using the drive method.
+    limo.drive(115)
+    # Mod 7: In your used_cars.py program, print your car object/s to make sure that the __str__ method is working
+    # as expected.
+    print(limo)
 
 
 main()
