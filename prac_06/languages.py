a6015172610f21d5ea8ac75a95b486f47bea049a
DamonLindsay <damonides@gmail.com>
2023-10-28 16:39:25 +1000

    Update programming_language.py with  __str__ method, fix bug with arguments taken by __init__

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,4 +1,11 @@
 """
-Expected Time: 60 mins (4:27pm start)
+Expected Time: 30 mins (4:27pm start)
 Actual Time:
 """
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
