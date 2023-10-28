313115bf58d0b71c939bf942c07720aa8645b534
DamonLindsay <damonides@gmail.com>
2023-10-28 16:45:34 +1000

    Change list to use proper variable names instead of separate object entities

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -11,9 +11,7 @@ visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
 print()  # Separate initial part of program from 2nd half.
 
-programming_languages = [ProgrammingLanguage("Python", "Dynamic", True, 1991),
-                         ProgrammingLanguage("Ruby", "Dynamic", True, 1995),
-                         ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
+programming_languages = [python, ruby, visual_basic]
 
 print("The dynamically typed languages are: ")
 for programming_language in programming_languages:
