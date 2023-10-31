970e6d8b4b052c0cde33fc451eb47efae588d86d
DamonLindsay <damonides@gmail.com>
2023-10-31 15:52:25 +1000

    Add myguitars.py with main, display_guitars and get_guitars functions.  Update guitar.py class module to handle 'less than' instances, also refactored is_vintage method

---
+++ prac_07/guitar.py
@@ -0,0 +1,34 @@
+"""
+Guitar class module
+"""
+CURRENT_YEAR = 2023
+
+
+class Guitar:
+    """Guitar class module"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize the Guitar object."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return the string version of the Guitar object."""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def __repr__(self):
+        """Return the string version of the Guitar object."""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def __lt__(self, other):
+        """Test for less than other based on the 'year' attribute."""
+        return self.year < other.year
+
+    def get_age(self):
+        """Return how old the guitar is in years."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return 'True' if the guitar is 50 or more years old."""
+        return self.get_age() >= 50
