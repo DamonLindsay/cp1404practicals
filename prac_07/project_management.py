956b832e69f66998308071cac8bf54f22edff582
DamonLindsay <damonides@gmail.com>
2023-10-31 20:24:07 +1000

    Create template for menu-based program

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -6,4 +6,35 @@ Actual Time:
 
 from project import Project
 
-print(Project("Build Car Park", "12/09/2021", 2, 60000.0, 95))
+MENU_INSTRUCTIONS = (
+    "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
+    "- (U)pdate project\n- (Q)uit")
+
+
+def main():
+    """This menu-based program will allow a user to interact with various projects and check their status,
+    as well as update them. """
+    print(MENU_INSTRUCTIONS)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("Load projects")
+        elif choice == "S":
+            print("Save projects")
+        elif choice == "D":
+            print("Display projects")
+        elif choice == "F":
+            print("Filter projects by date")
+        elif choice == "A":
+            print("Add new project")
+        elif choice == "U":
+            print("Update project")
+        else:
+            print("Invalid option.")
+        choice = input(">>> ").upper()
+    print("Thank you for using custom-built project management software.")
+
+
+main()
+
+# print(Project("Build Car Park", "12/09/2021", 2, 60000.0, 95))
