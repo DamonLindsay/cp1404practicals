db3572555a45ae90a112018f6a0be9522449b144
DamonLindsay <damonides@gmail.com>
2023-10-31 20:10:42 +1000

    Create the project.py class module with init, str and repr functionality

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,3 +1,24 @@
 """
-
+Project class module
 """
+
+
+class Project:
+    """The Project class."""
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0, completion_percentage=0):
+        """Initialize the Project object."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return the string version of the Project object."""
+        return (f"Name: {self.name}, Start Date: {self.start_date}, Priority: {self.priority}, "
+                f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}")
+
+    def __repr__(self):
+        """Return the X version of the Project object."""
+        return (f"Name: {self.name}, Start Date: {self.start_date}, Priority: {self.priority}, "
+                f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}")
---
+++ prac_07/project_management.py
@@ -0,0 +1,9 @@
+"""
+Project Management
+Expected Time: 120 minutes (8:03pm start)
+Actual Time:
+"""
+
+from project import Project
+
+print(Project("Build Car Park", "12/09/2021", 2, 60000.0, 95))
