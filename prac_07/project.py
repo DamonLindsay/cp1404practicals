dbc5590860fd695003fcbc1ccf5c656e7d1db96e
DamonLindsay <damonides@gmail.com>
2023-10-31 20:40:17 +1000

    Add initial load on program start of projects from file, as well as read_project_from_file function

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,6 +5,7 @@ Project class module
 
 class Project:
     """The Project class."""
+
     def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0, completion_percentage=0):
         """Initialize the Project object."""
         self.name = name
@@ -14,11 +15,11 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        """Return the string version of the Project object."""
+        """Return the string representation of the Project object."""
         return (f"Name: {self.name}, Start Date: {self.start_date}, Priority: {self.priority}, "
                 f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}")
 
     def __repr__(self):
-        """Return the X version of the Project object."""
+        """Return the string representation of the Project object."""  # TODO BETTER DOCSTRING?
         return (f"Name: {self.name}, Start Date: {self.start_date}, Priority: {self.priority}, "
                 f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}")
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,7 +5,9 @@ Actual Time:
 """
 
 from project import Project
+import datetime
 
+DEFAULT_FILENAME = "projects.txt"
 MENU_INSTRUCTIONS = (
     "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
     "- (U)pdate project\n- (Q)uit")
@@ -14,11 +16,16 @@ MENU_INSTRUCTIONS = (
 def main():
     """This menu-based program will allow a user to interact with various projects and check their status,
     as well as update them. """
+    projects = []
+
+    read_project_from_file(projects, DEFAULT_FILENAME)
+
     print(MENU_INSTRUCTIONS)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            print("Load projects")
+            filename = input("Enter Filename: ")
+            read_project_from_file(projects, filename)
         elif choice == "S":
             print("Save projects")
         elif choice == "D":
@@ -35,6 +42,19 @@ def main():
     print("Thank you for using custom-built project management software.")
 
 
-main()
+def read_project_from_file(projects, filename):
+    """Read project information from a file and populate a list of projects."""
+    with open(filename, "r", encoding="utf-8") as input_file:
+        input_file.readline()
+        for line in input_file:
+            parts = line.strip().split("\t")
+            name = parts[0]
+            start_date = parts[1]
+            priority = int(parts[2])
+            cost = float(parts[3])
+            percentage_completion = int(parts[4])
+            project = Project(name, start_date, priority, cost, percentage_completion)
+            projects.append(project)
 
-# print(Project("Build Car Park", "12/09/2021", 2, 60000.0, 95))
+
+main()
