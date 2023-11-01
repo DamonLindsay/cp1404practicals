2c2a12b65bb3576a35308991b68b65e42d00225c
DamonLindsay <damonides@gmail.com>
2023-11-01 11:53:03 +1000

    Update Project class docstrings to be more detailed

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,13 +1,13 @@
 """
-Project class module
+A class representing a Project object with its attributes.
 """
 
 
 class Project:
-    """The Project class."""
+    """A class representing a Project object with its attributes."""
 
     def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0, completion_percentage=0):
-        """Initialize the Project object."""
+        """Initialize the Project object with specified attributes."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -20,18 +20,18 @@ class Project:
                 f"Cost estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
 
     def __repr__(self):
-        """Return the string representation of the Project object."""  # TODO BETTER DOCSTRING?
+        """Return the string representation of the Project object for debugging and inspection purposes."""
         return (f"Name: {self.name}, Start Date: {self.start_date}, Priority: {self.priority}, "
                 f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}")
 
     def __lt__(self, other):
-        """Determine which project is less important based on its priority."""
+        """Determine if the current project is of lower priority than another project."""
         return self.priority < other.priority
 
     def __eq__(self, other):
-        """Determine which project is equally import based on its priority."""
+        """Determine if the current project has the same priority as another project."""
         return self.priority == other.priority
 
     def __gt__(self, other):
-        """Determine which project is more important based on its priority."""
+        """Determine if the current project is of higher priority than another project."""
         return self.priority > other.priority
