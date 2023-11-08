70af7ed0ab2ed4f800ad53f055785e9d38b51bc7
DamonLindsay <damonides@gmail.com>
2023-11-08 11:24:16 +1000

    Add first completed exercise of prac_08

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,15 +4,20 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Construct the app."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
-        """"""
+        """Handle changes to welcome new users."""
         # print("Greet test")
-        self.root.ids.output_label.text = "Hello "
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def press_clear(self):
+        """Clear any buttons and text."""
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = "Greet"
 
 
 BoxLayoutDemo().run()
