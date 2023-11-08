08640b67b78498e1f354406ec3ce536e95b22caa
DamonLindsay <damonides@gmail.com>
2023-11-08 12:08:55 +1000

    Add functionality to convert miles to km and have label show result

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,13 +1,28 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class ConvertMilesToKM(App):
+    """"""
+    message = StringProperty()
+
     def build(self):
         """Construct the app."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Enter your miles and press the Convert button to see in Kilometres."
         return self.root
+
+    def handle_update(self):
+        """Handle changes to the text input by updating the model from the view."""
+        miles = self.root.ids.user_input.text
+        kilometres = self.convert_miles_to_km(float(miles))
+        self.message = f"{kilometres}"
+
+    def convert_miles_to_km(self, miles):
+        """Convert miles to kilometres."""
+        return miles * 1.60934
 
 
 ConvertMilesToKM().run()
