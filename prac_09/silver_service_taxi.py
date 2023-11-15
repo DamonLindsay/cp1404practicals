26f5f4f62213002a52ff003972658805f174f86d
DamonLindsay <damonides@gmail.com>
2023-11-15 14:53:04 +1000

    Add 'SilverServiceTaxi' portion of prac_09

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,26 @@
+"""
+Silver Service Taxi Class Module
+Estimate: 20 minutes
+Actual: 23 Minutes
+"""
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    flagfall = 4.50
+
+    def __init__(self, name="", fuel=0, fanciness=0.0):
+        """Initialize the Silver Service Taxi object."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = fanciness * Taxi.price_per_km
+        self.current_fare_distance = 0
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.flagfall + (self.price_per_km * self.current_fare_distance)
+
+    def __str__(self):
+        """Return a string like a Taxi but with current fare distance and flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,13 @@
+"""
+Silver Service Taxi Test Module
+"""
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+test_taxi = SilverServiceTaxi("Test Taxi", 100, 2)
+
+print(test_taxi)
+print(f"Expected: 4.5, Actual: {test_taxi.get_fare()}")
+
+test_taxi.drive(18)
+print(test_taxi)
+print(F"Expected: 48.78, Actual: {test_taxi.get_fare()}")
